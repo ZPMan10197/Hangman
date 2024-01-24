1fb967708c5c534810764334beb95a5911735327
Zeshawn Manzoor <zeshawnmanzoor@yahoo.com>
2024-01-24 18:35:13 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def play(word):
                 tries -= 1
                 guessed_letters.append(guess)
             else:
-                print("Good job,", guess, "is in the word!")
+                print("Good job,", guess, "is in the word.")
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
                 indices = [i for i, letter in enumerate(word) if letter == guess]
